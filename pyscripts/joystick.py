b22bef6a89ce5b014e253bdd57f1ffa3529e32fa
Jay3112 <31185942+Jay3112@users.noreply.github.com>
2017-09-16 09:19:02 +0530

    Update joystick.py

--- pyscripts/joystick.py
+++ pyscripts/joystick.py
@@ -8,7 +8,11 @@ try:
     GPIO.setwarnings(False)
 
     GPIO.setmode(GPIO.BOARD)
-
+	GPIO.setup(3,GPIO.OUT)
+	GPIO.setup(5,GPIO.OUT)
+	GPIO.setup(7,GPIO.OUT)
+	GPIO.setup(11,GPIO.OUT)
+	
     # storing command line arguement for output logic
     # logic True or False
     top = bool(int(argv[1]))
@@ -18,6 +22,38 @@ try:
 
     print("Top: {}, Down: {}, Right: {}, Left: {}"
           .format(top, down, right, left))
-
+		  
+	if(top) :
+		GPIO.output(3,True)
+		GPIO.output(5,False)
+	elif(down) :
+		GPIO.output(5,True)
+		GPIO.output(3,False)
+	elif(top && right) :
+		GPIO.output(3,True)
+		GPIO.output(5,False)
+		GPIO.output(7,True)
+		GPIO.output(11,False)
+	elif(top && left) :
+		GPIO.output(3,True)
+		GPIO.output(5,False)
+		GPIO.output(7,False)
+		GPIO.output(11,True)
+	elif(down && right) :
+		GPIO.output(3,False)
+		GPIO.output(5,True)
+		GPIO.output(7,True)
+		GPIO.output(11,False)
+	elif(down && right) :
+		GPIO.output(3,False)
+		GPIO.output(5,True)
+		GPIO.output(7,False)
+		GPIO.output(11,True)
+	else :
+		GPIO.output(3,False)
+		GPIO.output(5,False)
+		GPIO.output(7,False)
+		GPIO.output(11,False)
+		
 except:
     print("Something went wrong!")
